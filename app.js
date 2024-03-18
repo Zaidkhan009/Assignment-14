8e0532bb65efad172da1dd58690c153664292c05
Zaid khan <160598429+Zaidkhan009@users.noreply.github.com>
2024-03-19 01:59:23 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Guest_list = void 0;
+let Guest_list = ['ahmer', 'azeem', 'atif', 'usama'];
+exports.Guest_list = Guest_list;
+for (let i = 0; i < Guest_list.length; i++) {
+    console.log('Respected Sir/Madam' + Guest_list[i] + '\nMe invite you on dinner tomorrow.\n\nThank You');
+}
---
+++ app.ts
@@ -0,0 +1,9 @@
+let Guest_list : string [] = ['ahmer','azeem','atif','usama'];
+for(let i=0; i<Guest_list.length; i++){
+  console.log('Respected Sir/Madam' + Guest_list[i] + '\nMe invite you on dinner tomorrow.\n\nThank You')
+}
+
+export{Guest_list};
+
+
+
